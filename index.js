f75b8a8dd22cc46ad3b07a13b73809170d79b09d
Tobey <primatjahja@gmail.com>
2023-10-10 16:14:36 +0800

    Add CSS link

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const port = 3000;
 
 //Step 4 - Add a dynamic year to the footer.
 //Hint: Google to find out how to get the current year using JS.
+app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
